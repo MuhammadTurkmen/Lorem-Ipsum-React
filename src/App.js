2085f81a5a65b4ca0eda247632179663bfe23239
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-07 14:06:31 +0430

    init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [text, setText] = useState([])
   const handleSubmit = (e) => {
     e.preventDefault()
+    console.log('hello world');
   }
   return (
     <section className='section-center'>
