123b3c30574d06fc98da708a3552fab9059f13b3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-07 14:02:42 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   const [count, setCount] = useState(0)
-  
+  const [text, setText] = useState([])
   return (
   <h2>lorem ipsum project setup</h2>
     ) 
