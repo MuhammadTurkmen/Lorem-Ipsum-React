804affec97c0c799161c6858bf92eb1c32ea13df
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-07 14:05:06 +0430

    init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState([])
   return (
-    <section className='section-center'></section>
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form'></form>
+    </section>
   ) 
 }
 
